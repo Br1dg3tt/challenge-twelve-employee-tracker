851fa179b833444fdf360ebe5e28a1168a955c97
Br1dg3tt <br1dg3tt3v3@gmail.com>
2024-11-20 18:10:05 -0700

    added more dependencies and files

---
+++ index.js
@@ -0,0 +1,8 @@
+// importing required packages
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const db = require('./db/connection');
+
+//importing dotenv package
+require('dotenv').config();
+
