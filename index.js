465c68a8b54588513d18f55aeaa4404f9d474416
Br1dg3tt <br1dg3tt3v3@gmail.com>
2024-11-20 18:32:55 -0700

    added input and update functions

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
         manager_id: {
             type: DataType.INTEGER
         }
-        tableName: 'Employees'
+        tableName: 'employees'
     });
 
     const Role = sequelize.define('Role', {
@@ -49,7 +49,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
         department_id: {
             type: DataType.INTEGER
         }
-        tableName: 'Roles'
+        tableName: 'roles'
     });
 
     const Manager = sequelize.define('Manager', {
@@ -67,7 +67,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
         department_id: {
             type: DataType.INTEGER
         }
-        tableName: 'Managers'
+        tableName: 'managers'
     });
 
     const Department = sequelize.define('Department', {
@@ -79,9 +79,153 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
         name: {
             type: DataType.STRING
         }
-        tableName: 'Departments'
+        tableName: 'departments'
     });
-    
+
+Employee.connectsTo(Role, {foreignKey: 'role_id', as: 'role'});
+Employee.connectsTo(Manager, {foreignKey: 'manager_id', as: 'manager'});
+Role.connectsTo(Department, {foreignKey: 'department_id', as: 'department'});
+Manager.connectsTo(Department, {foreignKey: 'department_id', as: 'department'});
+
+//creating the tables
+const input = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'home',
+        choices: ['View All Employees', 'View All Roles', 'View All Departments', 'Add Employee', 'Add Role', 'Add Department', 'Update Employee Role', 'Quit']
+    }
+]
+
+//function to view all employees
+const viewEmployees = () => {
+    Employee.findAll({
+        include: [
+            {
+                model: Role,
+                as: 'role'
+            },
+            {
+                model: Manager,
+                as: 'manager'
+            }
+        ]
+    }).then(employees => {
+        console.table(employees);
+    });
+};
+
+//function to view all roles
+const viewRoles = () => {
+    Role.findAll({
+        include: [
+            {
+                model: Department,
+                as: 'department'
+            }
+        ]
+    }).then(roles => {
+        console.table(roles);
+    });
+};
+
+//function to view all departments
+const viewDepartments = () => {
+    Department.findAll().then(departments => {
+        console.table(departments);
+    });
+};
+
+//function to add an employee
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter the employee\'s first name:',
+            name: 'first_name'
+        },
+        {
+            type: 'input',
+            message: 'Enter the employee\'s last name:',
+            name: 'last_name'
+        },
+        {
+            type: 'input',
+            message: 'Enter the employee\'s role ID:',
+            name: 'role_id'
+        },
+        {
+            type: 'input',
+            message: 'Enter the employee\'s manager ID:',
+            name: 'manager_id'
+        }
+    ]).then(employee => {
+        Employee.create(employee);
+    });
+};
+
+//function to add a role
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter the role title:',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Enter the role salary:',
+            name: 'salary'
+        },
+        {
+            type: 'input',
+            message: 'Enter the role department ID:',
+            name: 'department_id'
+        }
+    ]).then(role => {
+        Role.create(role);
+    });
+};
+
+//function to add a department
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter the department name:',
+            name: 'name'
+        }
+    ]).then(department => {
+        Department.create(department);
+    });
+};
+
+//function to update an employee's role
+const updateEmployeeRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter the employee ID:',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: 'Enter the new role ID:',
+            name: 'role_id'
+        }
+    ]).then(employee => {
+        Employee.update(employee, {
+            where: {
+                id: employee.id
+            }
+        });
+    });
+};
+
+//function to quit the application
+const quit = () => {
+    process.exit();
+};
 
 
 
