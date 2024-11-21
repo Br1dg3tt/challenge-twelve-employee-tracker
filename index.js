f2b57aec02c61563fe9327391a3466ce8b573230
Br1dg3tt <br1dg3tt3v3@gmail.com>
2024-11-20 18:18:31 -0700

    adding employee capture

--- index.js
+++ index.js
@@ -2,7 +2,39 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const db = require('./db/connection');
+const consoleTable = require('console.table');
 
 //importing dotenv package
 require('dotenv').config();
+
+//connecting to the database
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'postgres'
+    });
+
+    const Employee = sequelize.define('Employee', {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        first_name: {
+            type: DataType.STRING
+        },
+        last_name: {
+            type: DataType.STRING
+        },
+        role_id: {
+            type: DataType.INTEGER
+        },
+        manager_id: {
+            type: DataType.INTEGER
+        }
+        tableName: 'Employees'
+    });
+
+
+
+
 
