a477388cb001f003bab6881814ef78d391817dab
Br1dg3tt <br1dg3tt3v3@gmail.com>
2024-11-20 18:23:11 -0700

    added other categories

--- index.js
+++ index.js
@@ -34,6 +34,55 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
         tableName: 'Employees'
     });
 
+    const Role = sequelize.define('Role', {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataType.STRING
+        },
+        salary: {
+            type: DataType.DECIMAL
+        },
+        department_id: {
+            type: DataType.INTEGER
+        }
+        tableName: 'Roles'
+    });
+
+    const Manager = sequelize.define('Manager', {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        first_name: {
+            type: DataType.STRING
+        },
+        last_name: {
+            type: DataType.STRING
+        }
+        department_id: {
+            type: DataType.INTEGER
+        }
+        tableName: 'Managers'
+    });
+
+    const Department = sequelize.define('Department', {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataType.STRING
+        }
+        tableName: 'Departments'
+    });
+    
+
 
 
 
